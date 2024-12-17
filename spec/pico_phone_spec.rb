cd1d1742fc26e6daedb8998b994ad8f72b3e0a18
Gabi Jack <gabi@gabijack.com>
2024-12-17 09:29:04 -0500

    Add tests for PicoPhone module methods

--- spec/pico_phone_spec.rb
+++ spec/pico_phone_spec.rb
@@ -4,4 +4,70 @@ RSpec.describe PicoPhone do
   it "has a version number" do
     expect(PicoPhone::VERSION).not_to be nil
   end
+
+  it "stores a default_country" do
+    PicoPhone.default_country = "US"
+
+    expect(PicoPhone.default_country).to eq("US")
+  end
+
+  it "stores a default extension prefix" do
+    PicoPhone.default_extension_prefix = "ext."
+
+    expect(PicoPhone.instance_variable_get(:@default_extn_prefix)).to eq("ext.")
+  end
+
+  describe "possible?" do
+    it "returns false for a string that is not a phone number" do
+      expect(PicoPhone.possible?("Not a number")).to be false
+    end
+
+    it "returns true for a string that contains a phone number among other words" do
+      expect(PicoPhone.possible?("This is my phone: 5102745656")).to be true
+    end
+  end
+
+  describe "possible_for_country?" do
+    it "returns false for numbers that cannot represent a phone number in the country" do
+      expect(PicoPhone.possible_for_country?("208765432", "US")).to be false
+    end
+
+    it "returnst true for numbers that could represent a phone number in the country" do
+      expect(PicoPhone.possible_for_country?("5102745556", "US")).to be true
+    end
+  end
+
+  describe "valid?" do
+    before do
+      PicoPhone.default_country = "US"
+    end
+
+    it "returns false for a number that is not valid in the default country" do
+      expect(PicoPhone.valid?("0435582008")).to be false
+    end
+
+    it "returns true for a number that is valid in the default country" do
+      expect(PicoPhone.valid?("5102745656")).to be true
+    end
+  end
+
+  describe "valid_for_country?" do
+    before do
+      PicoPhone.default_country = "US"
+    end
+
+    it "returns false for a number that is not valid in the specified country" do
+      expect(PicoPhone.valid_for_country?("0435582008", "US")).to be false
+    end
+
+    it "returns true for a number that is valid in the specified country" do
+      expect(PicoPhone.valid_for_country?("0435582008", "AU")).to be true
+    end
+  end
+
+  describe "parse" do
+    it "returns an instance of PhoneNumber" do
+      expect(PicoPhone.parse("5102743434")).to be_a(PicoPhone::PhoneNumber)
+    end
+  end
 end
